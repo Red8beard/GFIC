f4ae8c9a51379dda018d47def0972874ca727305
Jeremy Davidson <53384787+Red8beard@users.noreply.github.com>
2020-04-12 20:20:23 -0400

    Delete test.py

--- test.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.DataFrame({'Articles':['1254', '55131', '45321', '893210'], 'Description':['Chandail', 'tuque', 'bobette', 'Hoodie'], 'Quantite':['1', '2', '4', '1'], 'Prix':['60', '40', '30', '70']})
-
-print(df)
